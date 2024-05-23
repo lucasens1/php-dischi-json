5ae99536d1eb83a8db78c2c6488ebc00e0cfe553
lucasens1 <lucasensini1@gmail.com>
2024-05-23 15:49:15 +0200

    approaching webapp, post server

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+
+createApp({
+    Data(){
+        return{
+            diskList : [],
+            isFav : false
+        }
+    },
+    created(){
+        axios.get("http://localhost/boolean/php-dischi-json/server.php").then((r) => {
+            /* console.log(r); */
+            this.diskList = r.data.disks;
+            /* console.log(this.diskList); */
+        });
+    },
+    methods : {
+
+    }
+}).mount("#app");
