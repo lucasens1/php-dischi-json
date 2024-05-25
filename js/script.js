458318f99d4266934fcbd4c4e66de241f9f40414
lucasens1 <lucasensini1@gmail.com>
2024-05-25 19:42:30 +0200

    excercise completed

--- js/script.js
+++ js/script.js
@@ -4,20 +4,29 @@ createApp({
     data(){
         return{
             diskList : [],
+            /* Da togliere */
             liked_disks : [],
+            filtered_disks : [],
+            option : "all",
             my_api : "http://localhost/boolean/php-dischi-json/server.php"
         }
     },
     created(){
-        axios.get(this.my_api).then((r) => {
-            /* console.log(r); */
-            this.diskList = r.data.disks;
-            this.liked_disks = r.data.likedisks;
-            console.log(this.diskList);
-            console.log(this.liked_disks);
-        });
+        this.initializeData();
     },
     methods: {
+        initializeData(){
+            axios.get(this.my_api).then((r) => {
+                /* console.log(r); */
+                this.diskList = r.data.disks;
+                this.liked_disks = r.data.likedisks;
+                this.populateFilterDisks();
+                console.log(this.filtered_disks);
+                console.log(this.diskList);
+                /* Da togliere */
+                console.log(this.liked_disks);
+            });
+        },
         likeUnlike(i) {
             console.log(i);
             const data = {
@@ -32,7 +41,21 @@ createApp({
             }).then((r) => {
                 this.liked_disks = r.data.likedisks;
                 console.log(this.liked_disks);
+                this.populateFilterDisks();
             });
         },
+        populateFilterDisks() {
+            //Controllo se l'opzione è per i preferiti
+            if (this.option === "liked_ones") {
+                //filter per popolare filtered_disks solo se isLiked è true
+                this.filtered_disks = this.diskList.filter(disk => disk.isLiked);
+            } else {
+                this.filtered_disks = this.diskList;
+            }
+        },
+        handleChange(){
+            console.log("Sto cambiando..");
+            this.populateFilterDisks();
+        }
     },
 }).mount("#app");
